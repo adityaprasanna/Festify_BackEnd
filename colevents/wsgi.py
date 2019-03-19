8a017009cf04d02a970ad2be2535394077f36f3a
Swapnil Kumar <swapnil.kumar@myntra.com>
2019-03-20 01:15:13 +0530

    asd.kja;sd

--- colevents/wsgi.py
+++ colevents/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append('/home/ubuntu/colfest/swapnilcolfest/Festify_BackEnd2/colevents')
 # sys.path.append('/home/ubuntu/colfest/swapnilcolfest/Festify_BackEnd2/colevents')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "colevents.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_wsgi_application()
